8b00b9165fbdeac46e730ff09ec99d50ff55dbb6
Denes Sentmartoni <denessentmartoni.com>
2024-02-05 18:51:04 +0100

    Added a Student class, and the actions Post and Get

---
+++ WebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private static List<Student> students = new List<Student>() {
+            new Student { Indeks="PR1/2019", FirstName = "Milan", LastName="Milanovic", studyYear=3 },
+            new Student { Indeks="PR2/2019", FirstName = "Milos", LastName="Milosevic", studyYear=3 },
+            new Student { Indeks="PR3/2019", FirstName = "Marko", LastName="Markovic", studyYear=3 },
+            new Student { Indeks="PR4/2019", FirstName = "Milorad", LastName="Miloradovic", studyYear=3 }};
+
+
+        //  CRUD OPERATIONS
+
+        [HttpPost("upload")]
+        public ActionResult Upload([FromBody] Student student)
+        {
+            try {
+                students.Add(student);
+                return Ok(new { Message = "Successful upload!" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { Error = ex.Message });
+            }
+        }
+
+        [HttpGet("all")]
+        public ActionResult GetAll()
+        {
+            try
+            {
+                return Ok(new { Studenst = students });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { Error = ex.Message });
+            }
+        }
+
+    }
+}
---
+++ WebAPI/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPI.Models
+{
+    public class Student
+    {
+        public string Indeks { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int studyYear { get; set; }
+    }
+}
